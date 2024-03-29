b6a7aa87cfeea32befcebd0a1205f6d02a0d3aeb
Cee-Ya <100188425+Cee-Ya@users.noreply.github.com>
2024-03-29 20:56:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.13.2/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-Diy'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+sed -i "s/OpenWrt /Yarns build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
